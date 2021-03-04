dda3b8b6fd1d610611872c96d6727672e0a7b6ef
Tyler <4926565+tyler71@users.noreply.github.com>
2021-03-04 11:25:59 -0800

    Remove a lot of cruft
    Also use docker-compose healthchecks instead of in the entrypoint for waiting for database to load up
    Much better!

--- docker/forum_config/forum_directories.sh
+++ docker/forum_config/forum_directories.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-
+cd /app
 if [ -f config.php ]; then
-  while [ "$(php flarum 2>&1 | grep -q 'Uncaught PDOException'; echo $?)" -eq 0 ]; do
-    sleep 2
-  done
-  echo "Database up, attempting to migrate and cache assets"
   php flarum migrate
   # Requires Cache Assets
   # https://discuss.flarum.org/d/23321-cache-assets-by-bokt
@@ -32,9 +28,6 @@ adminUser:
   email: admin@example.com
 settings:
 EOF
-  while [ "$(curl --silent db:3306 &> /dev/null; echo $?)" -ne 56 ]; do sleep 2; done
-  echo "Database up, attempting to install"
-
   php flarum install --file install.yml
 fi
 
