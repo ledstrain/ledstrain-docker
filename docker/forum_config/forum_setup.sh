1217930c7526e20c9fcbf473b3a436bafb0e1788
Tyler <4926565+tyler71@users.noreply.github.com>
2021-03-04 15:11:00 -0800

    Runs with new instance
    Able to docker-compose down and back up
    Sessions are kept

--- docker/forum_config/forum_setup.sh
+++ docker/forum_config/forum_setup.sh
@@ -2,22 +2,28 @@
 set -e
 cd "$APPLICATION_PATH"
 
-mkdir -p /app/assets /app/public/assets /app/storage
-chown application -R /app/assets /app/public/assets /app/storage
+mkdir -p \
+ /app/assets         \
+ /app/public/assets  \
+ /app/storage
+chown application -R \
+  /app/assets        \
+  /app/public/assets \
+  /app/storage
 
 if [ -f /conf/config.php ]; then
   ln -s /conf/config.php ./config.php
   php flarum migrate
   # Requires Cache Assets
   # https://discuss.flarum.org/d/23321-cache-assets-by-bokt
-  php flarum cache:assets --js --css --locales || true
   php flarum cache:clear
+  php flarum cache:assets --js --css --locales || true
 
-  # Remove old views on load
-  # https://discuss.flarum.org/d/25436-beta-14-call-to-undefined-function-array-get/51
-  if [ -n "$(ls -A storage/views)" ]; then
-    rm -r storage/views/*
-  fi
+# # Remove old views on load
+# # https://discuss.flarum.org/d/25436-beta-14-call-to-undefined-function-array-get/51
+# if [ -n "$(ls -A storage/views)" ]; then
+#   rm -r storage/views/*
+# fi
 else
   cat > install.yml <<EOF
 debug: true
