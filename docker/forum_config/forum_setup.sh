7333d4314ac2b8ac999013214ef29d413c28f256
Tyler <4926565+tyler71@users.noreply.github.com>
2021-03-04 16:57:07 -0800

    Can load a existing forum
    Also build and push to registry

--- docker/forum_config/forum_setup.sh
+++ docker/forum_config/forum_setup.sh
@@ -6,25 +6,16 @@ mkdir -p \
  /app/assets         \
  /app/public/assets  \
  /app/storage
-chown application -R \
-  /app/assets        \
-  /app/public/assets \
-  /app/storage
 
 if [ -f /conf/config.php ]; then
-  ln -s /conf/config.php ./config.php
+  ln -fs /conf/config.php ./config.php
   php flarum migrate
   # Requires Cache Assets
   # https://discuss.flarum.org/d/23321-cache-assets-by-bokt
   php flarum cache:clear
   php flarum cache:assets --js --css --locales || true
 
-# # Remove old views on load
-# # https://discuss.flarum.org/d/25436-beta-14-call-to-undefined-function-array-get/51
-# if [ -n "$(ls -A storage/views)" ]; then
-#   rm -r storage/views/*
-# fi
-else
+elif [ "$INSTALL" == "true" ]; then
   cat > install.yml <<EOF
 debug: true
 baseUrl: ${HOSTNAME}
@@ -47,3 +38,7 @@ EOF
   chown application /conf/config.php
 fi
 
+chown application:application -R \
+  /app/assets        \
+  /app/public/assets \
+  /app/storage
