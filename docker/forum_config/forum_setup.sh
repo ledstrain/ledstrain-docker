365da201ba8e23515c960c3274cce957e2cd3aad
Tyler <4926565+tyler71@users.noreply.github.com>
2021-03-15 07:11:53 -0700

    Optimize caching and layers
    Most of the COPY's for image overriding are moved to the builder stage.
    Then one COPY copies all the changes over. This removes about 8 layers that rarely ever change.
    Moved the dev EXTIVERSE arg to just before it is used
    Also optimized the order so most layers are kept cached
    refactoring to make everything more understandable

--- docker/forum_config/forum_setup.sh
+++ docker/forum_config/forum_setup.sh
@@ -2,23 +2,7 @@
 set -e
 cd "$APPLICATION_PATH"
 
-mkdir -p \
-  /app/public/assets/avatars    \
-  /app/public/assets/extensions \
-  /app/public/assets/files      \
-  /app/public/assets/images     \
-  /app/storage
-
-if [ -f /conf/config.php ]; then
-  ln -fs /conf/config.php ./config.php
-  php flarum migrate
-  # Requires Cache Assets
-  # https://discuss.flarum.org/d/23321-cache-assets-by-bokt
-
-  php flarum cache:clear
-  php flarum cache:assets --js --css --locales || true
-
-elif [ "$INSTALL" == "true" ]; then
+install_flarum() {
   cat > install.yml <<EOF
 debug: true
 baseUrl: ${HOSTNAME}
@@ -39,6 +23,27 @@ EOF
   php flarum install --file install.yml
   cp config.php /conf/config.php
   chown "${PUID_ID}" /conf/config.php
+}
+
+
+mkdir -p \
+  /app/public/assets/avatars    \
+  /app/public/assets/extensions \
+  /app/public/assets/files      \
+  /app/public/assets/images     \
+  /app/storage
+
+if [ -f /conf/config.php ]; then
+  ln -fs /conf/config.php ./config.php
+  php flarum migrate
+
+  # Requires Cache Assets
+  # https://discuss.flarum.org/d/23321-cache-assets-by-bokt
+  php flarum cache:clear
+  php flarum cache:assets --js --css --locales || true
+
+elif [ "$INSTALL" == "true" ]; then
+  install_flarum
 fi
 
 chown "${PUID_ID}" -R                   \
