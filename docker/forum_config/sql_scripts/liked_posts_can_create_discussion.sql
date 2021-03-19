b999f8c494062ca16434f876947c57e0874f0f1c
Tyler <4926565+tyler71@users.noreply.github.com>
2021-03-19 12:06:18 -0700

    additional requirments for user like post
    the account must be at least 2 days old
    not suspended
    Also reordering for optimization

--- docker/forum_config/sql_scripts/liked_posts_can_create_discussion.sql
+++ docker/forum_config/sql_scripts/liked_posts_can_create_discussion.sql
@@ -1,17 +1,23 @@
 -- If a user has
 --  - Last seen in the last week
 --  - Is not approved for creating a post or discussion
---  - a post with at least 1 like from another user
+--  - A post with at least 1 like from another user
+--  - Account must be at least 2 days old
+--  - Account must not be suspended
 -- Then it will set approval to 1
 -- tldr; If someone else liked your post, you can create discussions without needing
 --  approval
 
-UPDATE users u SET first_post_approval_count=1, first_discussion_approval_count=1
+UPDATE users u SET first_post_approval_count=1,
+                   first_discussion_approval_count=1
  WHERE u.last_seen_at > (NOW() - INTERVAL 7 DAY)
-  AND u.id IN (SELECT p.user_id FROM posts p
-               WHERE p.id IN (SELECT post_id FROM post_likes pl WHERE pl.user_id != u.id)
-              )
+  AND  u.joined_at    < (NOW() - INTERVAL 2 DAY)
+  AND  u.suspended_until IS NULL
   AND (
           u.first_post_approval_count       = 0
        OR u.first_discussion_approval_count = 0
       )
+  AND  u.id IN (SELECT p.user_id FROM posts p
+                WHERE p.id IN (SELECT post_id FROM post_likes pl
+                               WHERE pl.user_id != u.id)
+               )
