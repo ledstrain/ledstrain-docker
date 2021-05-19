fcfc321669daa3aa48131fb03b2da34e6dbd9c3b
Tyler <4926565+tyler71@users.noreply.github.com>
2021-05-19 11:34:17 -0700

    Remove unused accounts from being a valid user

--- docker/forum_config/sql_scripts/50-create_valid_users.sql
+++ docker/forum_config/sql_scripts/50-create_valid_users.sql
@@ -1,10 +1,20 @@
--- Used to promote users to a group to allow extras like PMing
-
--- Should have a specific group for this.
--- We use the one named "Valid User"
--- It also uses the first post plugin column, first_discussion_approval_count
+-- We use the one named "Valid User" or valid
 SET @validUserGroup=(SELECT id FROM groups WHERE name_singular='Valid User');
+
+-- Used to promote users to a group to allow extras like PMing
+-- Should have a specific group for this.
+-- It also uses the first post plugin column, first_discussion_approval_count
 INSERT IGNORE INTO group_user (user_id, group_id)
   SELECT id, @validUserGroup FROM users u
   WHERE u.first_discussion_approval_count > 0
-    AND u.id NOT IN (SELECT user_id FROM group_user WHERE group_id = @validUserGroup)
+    AND u.id NOT IN (SELECT user_id FROM group_user WHERE group_id = @validUserGroup);
+
+-- If a user
+-- - Has not been active for 6 months
+-- - Has fewer then 10 posts
+-- Then they are removed from the valid group.
+DELETE FROM group_user gu
+  WHERE gu.group_id = @validUserGroup
+    AND gu.user_id IN (SELECT id FROM users u
+                         WHERE u.last_seen_at < (NOW() - INTERVAL 6 MONTH)
+                           AND u.comment_count < 10);
