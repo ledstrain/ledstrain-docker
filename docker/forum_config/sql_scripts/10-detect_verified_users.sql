32361873f993310eaac3bc6f3b16af374694bc7e
Tyler <4926565+tyler71@users.noreply.github.com>
2021-09-13 21:46:02 -0700

    Remove dependence on first  post column
    Use custom verified_user column
    Refactor scripts

---
+++ docker/forum_config/sql_scripts/10-detect_verified_users.sql
@@ -0,0 +1,29 @@
+-- If a user has
+--  - Been seen in the last week
+--  - An account that is at least 2 days old
+--  - An account that is not be suspended
+--  - Not already been approved for creating a post or discussion
+
+-- Then, any of these conditions
+--  - Created 5 posts
+--  - A post with at least 1 like from another user
+--  - A post that another user has mentioned.
+-- Then it will set approval to 1
+-- tldr; If someone else liked or replied to your post, you can
+-- create discussions without needing approval
+
+UPDATE users u SET verified_user=1,
+                   first_post_approval_count=1,
+                   first_discussion_approval_count=1
+ WHERE verified_user = 0
+  AND  u.last_seen_at > (NOW() - INTERVAL 7 DAY)
+  AND  u.joined_at    < (NOW() - INTERVAL 2 DAY)
+  AND  u.suspended_until IS NULL
+  AND  ( u.comment_count >= 5
+      OR u.id IN (SELECT p.user_id FROM posts p
+                   WHERE p.id IN (SELECT post_id FROM post_likes pl
+                                   WHERE pl.user_id != u.id
+                                  UNION ALL
+                                  SELECT mentions_post_id FROM post_mentions_post pmp
+                                   WHERE pmp.post_id IN (SELECT id FROM posts WHERE id != u.id)))
+       );
--- docker/forum_config/sql_scripts/50-add_to_verified_users_group.sql
+++ docker/forum_config/sql_scripts/50-add_to_verified_users_group.sql
@@ -1,19 +1,19 @@
--- We use the group named "Valid User" aka valid to for users to have extra capabilities.
-SET @validUserGroup=(SELECT id FROM groups WHERE name_singular='Valid User');
+-- We use the group named "Verified User" aka verified to for users to have extra capabilities.
+SET @verifiedUserGroup=(SELECT id FROM groups WHERE name_singular='Verified User');
 
 -- Used to promote users to a group to allow extras like PMing
 -- It uses the first post plugin column, first_discussion_approval_count
 INSERT IGNORE INTO group_user (user_id, group_id)
-  SELECT id, @validUserGroup FROM users u
-  WHERE u.first_discussion_approval_count > 0
-    AND u.id NOT IN (SELECT user_id FROM group_user WHERE group_id = @validUserGroup);
+  SELECT id, @verifiedUserGroup FROM users u
+  WHERE u.verified_user = 1
+    AND u.id NOT IN (SELECT user_id FROM group_user WHERE group_id = @verifiedUserGroup);
 
 -- If a user
 -- - Has not been active for 6 months
 -- - Has fewer then 10 posts
--- Then they are removed from the valid group.
+-- Then they are removed from the verified group.
 DELETE FROM group_user
-  WHERE group_user.group_id = @validUserGroup
+  WHERE group_user.group_id = @verifiedUserGroup
     AND group_user.user_id IN (SELECT id FROM users u
                          WHERE u.last_seen_at < (NOW() - INTERVAL 6 MONTH)
                            AND u.comment_count < 10);
