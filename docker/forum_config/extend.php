3c64429da4cd3c6e9a8166540c100bda48264fb0
Tyler <4926565+tyler71@users.noreply.github.com>
2025-09-23 14:23:02 -0700

    For new users, only permit links from specific locations

--- docker/forum_config/extend.php
+++ docker/forum_config/extend.php
@@ -22,19 +22,16 @@ return [
         // even a link works, only the domain will be used
         // ->allowLinksFromDomain('discuss.flarum.org/d/26095')
         // Alternatively, use an array of domains
-        ->allowLinksFromDomains([
-            'ledstrain.org',
-            'wiki.ledstrain.org'
-        ])
+      ->allowLinksFromDomains(json_decode(file_get_contents(__DIR__.'/new_user_allowed_domains.json')))
         // Use custom (expert) logic.
         // Return true to ignore further checking this link for validity.
         // ->allowLink(function (Uri $uri, User $actor = null) {
         //     if ($uri->getHost() === '127.0.0.1') return true;
         // })
         // How long after sign up all posts are scrutinized for bad content
-        ->checkForUserUpToHoursSinceSignUp(48)
+        // ->checkForUserUpToHoursSinceSignUp(48)
         // How many of the first posts of a user to scrutinize for bad content
-        ->checkForUserUpToPostContribution(2)
+        ->checkForUserUpToPostContribution(4)
         // Specify the user Id of the moderator raising flags for some actions, otherwise the first admin is used
         // ->moderateAsUser(2)
         // Disable specific spam prevention components
